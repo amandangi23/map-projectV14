5992b1b33c89041f5a2209aa07f05f7bf641d956
Vinay Dangi <vinaydangi145@gmail.com>
2023-10-06 12:47:38 +0530

    Map deployed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GooglemapComponent } from './components/map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { FormsModule } from '@angular/forms';
+import { SearchLocationComponent } from './components/search-location/search-location.component';
+import { GoogleMapComponent } from './components/google-map/google-map.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GooglemapComponent
+    AppComponent, 
+    SearchLocationComponent, GoogleMapComponent,
   ],
   imports: [
     BrowserModule,
