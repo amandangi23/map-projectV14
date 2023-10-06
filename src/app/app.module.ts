0072aefdc1fad6205ab90c675c6a77ab3d609173
amandangi23 <amandangi2723@gmail.com>
2023-10-06 12:51:16 +0530

    commit changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GooglemapComponent } from './components/map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    GooglemapComponent
+    
   ],
   imports: [
     BrowserModule,
