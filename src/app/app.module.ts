35f256a5da92ceda103ea88236d0d1868f864bb2
amandangi23 <amandangi2723@gmail.com>
2023-10-06 11:11:43 +0530

    changes updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { GooglemapComponent } from './components/map/map.component';
 import { GoogleMapsModule } from '@angular/google-maps'
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     GoogleMapsModule ,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [], 
   bootstrap: [AppComponent]
