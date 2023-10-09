c445f2d69742a797aa75c60fa462195f3b91c85e
Vinay Dangi <vinaydangi145@gmail.com>
2023-10-09 13:07:03 +0530

    changes update

--- src/app/components/current-location/current-location.component.ts
+++ src/app/components/current-location/current-location.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MapService } from 'src/app/services/map.service';
 
 @Component({
@@ -7,27 +7,73 @@ import { MapService } from 'src/app/services/map.service';
   styleUrls: ['./current-location.component.css']
 })
 export class CurrentLocationComponent implements OnInit  {
+  locationName: string | undefined;
+  userLocation: { lat: number; lng: number } | undefined;
+ 
 
   constructor(private mapService: MapService){}
 
+
   ngOnInit(): void {
-     
+    
   }
 
-  UserLocation() {
-    this.mapService.getUserLocation().then((location) => {
-      // Mark the user's location on the map
-      const userLatLng = new google.maps.LatLng(location.lat, location.lng);
-      const marker = new google.maps.Marker({
-        position: userLatLng,
-        map: this.mapService.getMap(),
-        title: 'Your Location'
+    
+  getUserLocation() {
+    if ('geolocation' in navigator) {
+      const options = {
+        enableHighAccuracy: true, // Request high accuracy mode
+        timeout: 10000, // Maximum time to wait for location (in milliseconds)
+        maximumAge: 0 // Disable cache, always get fresh location
+      };
+
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Handle the position data
+          const userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+          // Check if a user location marker exists, and remove it if it does
+          if (this.mapService.userLocationMarker) {
+            this.mapService.userLocationMarker.setMap(null);
+          }
+
+          // Create a new user location marker
+          this.mapService.userLocationMarker = new google.maps.Marker({
+            position: userLatLng,
+            map: this.mapService.getMap(),
+            title: 'Your Location'
+          });
+
+          // Center the map on the user's location
+          this.mapService.getMap().setCenter(userLatLng);
+          this.mapService.getMap().setZoom(15);
+        },
+        (error) => {
+          // Handle errors
+          console.error('Error getting user location:', error);
+        },
+        options // Pass the options here
+      );
+    } else { 
+      console.error('Geolocation is not supported by your browser.');
+    }
+  }   
+
+  
+          
+  
+  reverseGeocode(lat: number, lng: number) {
+    this.mapService
+      .reverseGeocode(lat, lng)
+      .then((name) => {
+        this.locationName = name;
+      })
+      .catch((error) => {
+        console.error('Error reverse geocoding:', error);
       });
-      this.mapService.getMap().setCenter(userLatLng);
-      this.mapService.getMap().setZoom(15);
-    }).catch((error) => {
-      console.error('Error getting user location:', error);
-    });
   }
+
+  
+  
 }
 
